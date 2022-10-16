d18134cf03e4bf9c5bcffb7406a6cd7c7880590d
Njiraini23 <njiraini88@gmail.com>
2022-10-16 12:18:59 -0700

    printif

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
-*main -print C program using printif function
+* main-print a prograusing printf
 *
-*Return:0
+* Return:0
 */
 int main(void)
 {
-	printf"with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
