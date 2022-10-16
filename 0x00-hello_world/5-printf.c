9b288157ce019b17c298b1861941f7f76987d0e1
Njiraini23 <njiraini88@gmail.com>
2022-10-16 11:45:14 -0700

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+*main -print C program using printif function
+*
+*Return:0
+*/
+int main(void)
+{
+	printf"with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
