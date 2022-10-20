8c90cc29147661a324e69777e7f05a817778e787
Njiraini23 <njiraini88@gmail.com>
2022-10-20 13:53:37 -0700

    prints up to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*print_to_98 -prints allnatural numbers
+*up to 98 separate them by comma and space
+*@n: thenumber to start
+*/
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
