4d7ddfd854b8ec33defcfe85190a18ac13fcbd35
Njiraini23 <njiraini88@gmail.com>
2022-10-20 07:06:46 -0700

    asalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -6,9 +6,11 @@
 *Return: I if char is letter lower or upper, 0 otherwise
 */
 int _isalpha(int c)
+{
 
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+
 		return (1);
 	else
 		return (0);
-
+}
