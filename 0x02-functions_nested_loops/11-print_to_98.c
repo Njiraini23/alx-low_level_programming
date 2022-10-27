6eeabb2e8eb2e3a3cbf56153cbcc7864d3c7917b
Njiraini23 <njiraini88@gmail.com>
2022-10-27 12:13:14 -0700

    Prints to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,20 +7,7 @@
 */
 void print_to_98(int n)
 {
-	if (n < 98)
-	{
-		while (n <= 98)
-		{
-			printf("%d", n);
-			if (n != 98)
-			{
-				printf(", ");
-			}
-			n++;
-		}
-	}
-
-	else if (n < 98)
+	if (n > 98)
 	{
 		while (n >= 98)
 		{
@@ -32,7 +19,21 @@ void print_to_98(int n)
 			n--;
 		}
 	}
+
 	else
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+	}
+	printf("\n");
+}
 	{
 		printf("98");
 	}
