62fb8b93d787712125602a23131b294b07338d9b
Njiraini23 <njiraini88@gmail.com>
2022-10-30 12:46:48 -0700

    encode a string to 1337

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* leet -write a code instead of 1337
+* @s: the string to be chnaged
+*
+*Return: address of s
+*/
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; *(s + i); i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (a[j] == s[i])
+				s[i] = b[j];
+		}
+	}
+	return (s);
+}
