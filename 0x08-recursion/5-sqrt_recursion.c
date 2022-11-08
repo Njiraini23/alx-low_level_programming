3f82fdba753e3de77de694830dff608ae5f3a3e2
Njiraini23 <njiraini88@gmail.com>
2022-11-08 01:11:02 -0800

    sqrt

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,23 +3,23 @@
 * check -square root
 * @a:int
 * @b:int
-* _sqrt_recursion - returns the natural square root of a number
 *
 *Return: int
 */
 int check(int a, int b)
 {
 	if (a * a == b)
-	{
+	
 		return (a);
-	}
+	
 	if (a * a > b)
-	{
+	
 		return (-1);
 
 		return (check(a + 1, b));
-	}
+	
 }
+
 /**
 * _sqrt_recursion -Find the square root of a number
 *@n:square root integer
