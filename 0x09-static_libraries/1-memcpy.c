20d9fb661feec995b714273b55ab948d7a0cb1bd
Njiraini23 <njiraini88@gmail.com>
2022-11-10 10:33:26 -0800

    Memory copying

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,9 +1,20 @@
 #include "main.h"
 /**
-*_memset - fill the memory with a constant byte
-*@s: the starting point of a memory
-*@b: the desired value of the memory
-*@n:number of bytes to be changed
-*Return:
+*_memcpy - copy a memory area
+*@dest: the destination where it is stored
+*@src: the source where memory is copied from
+*@n:number of bytes
+*Return: the copied bytes
 */
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int r = 0;
+	int i = n;
+
+	for (; r < i; r++)
+	{
+		dest[r] = src[r]
+			n--;
+	}
+	return (dest);
+}
