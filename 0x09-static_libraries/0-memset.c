0c7e2069b95975e477aa484f6252b87683ae83b4
Njiraini23 <njiraini88@gmail.com>
2022-11-10 07:21:55 -0800

    memset

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*_memset - fill the memory with a constant byte
+*@s: the starting point of a memory
+*@b: the desired value of the memory
+*@n:number of bytes to be changed
+*Return:The chnaged value
+*/
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--
+	}
+	return (s);
+}
---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+*_memset - fill the memory with a constant byte
+*@s: the starting point of a memory
+*@b: the desired value of the memory
+*@n:number of bytes to be changed
+*Return:
+*/
+char *_memset(char *s, char b, unsigned int n)
