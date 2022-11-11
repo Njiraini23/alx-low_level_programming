4b451f2a3db46d12dcbce5fb2ebfd80350c20193
Njiraini23 <njiraini88@gmail.com>
2022-11-11 13:39:51 +0000

    puts

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+void _puts(char *str)
+/**
+*puts -inserts a line
+* main - entry point
+*Return: Always 0
+*
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
+
