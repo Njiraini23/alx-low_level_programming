c98d4ddd9a2248a82437493fa946f404e8a324b7
Njiraini23 <njiraini88@gmail.com>
2022-11-17 12:43:14 -0800

    my name is

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+*main - the main argument
+*@argc: the argument count
+*@argv: arguments
+*
+*Return: Always 0;
+*/
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
