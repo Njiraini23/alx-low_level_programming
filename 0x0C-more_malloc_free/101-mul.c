397b65eeb1602078423eaad23165bb4feb8c1913
Njiraini23 <njiraini88@gmail.com>
2022-11-17 07:28:41 -0800

    the extra task

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+*_print -to mutliply two numbers
+*@str: a string
+*@l: the length of a string
+*
+*Return: void
+*/
+void _print(char *str, int l)
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < l)
+	{
+		if (str[i] != '0')
+			j = 1;
+		if (j || i == l - 1)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar ('\n');
+	free(str);
+}
+/**
+*mul -multiplies the character with a string
+*@n: the character to multiply
+*@num: string to multiply
+*@num_index: the last number of the null index
+*@dest: the destination of the multiplication
+*@dest_index: the index of the destination
+*
+*Return: the pointer to the dest
+*/
+char *mul(char n, char *num, int num_index, char *dest, int dest_index)
+{
+	int j, k, mul, mulrem, add, addrem;
+
+	mulrem = addrem = 0;
+	for (j = num_index, k = dest_index; j >= 0; j--, k--)
+	{
+		mul = (n - '0') * (num[j] - '0') + mulrem;
+		mulrem = mul / 10;
+		add = (dest[k] - '0') + (mul % 10) + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	for (addrem += mulrem; k >= 0 && addrem; k--)
+	{
+		add = (dest[k] - '0') + addrem;
+		addrem = add / 10;
+		dest[k] = add % 10 + '0';
+	}
+	if (addrem)
+	{
+		return (NULL);
+	}
+	return (dest);
+}
