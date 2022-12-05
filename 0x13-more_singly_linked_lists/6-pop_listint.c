bb9db92f961ba317292cb982a21acf521c82519c
Njiraini23 <njiraini88@gmail.com>
2022-12-05 20:22:09 +0000

    Betty styling

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -12,7 +12,7 @@ int pop_listint(listint_t **head)
 	listint_t *start;
 	int n;
 
-	if(head == NULL || *head == NULL)
+	if (head == NULL || *head == NULL)
 		return (0);
 	start = *head;
 	*head = start->next;
