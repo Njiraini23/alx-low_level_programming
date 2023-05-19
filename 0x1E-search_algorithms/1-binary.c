8c059bbc5f8cb994bfb0ad909babce8bd06eed46
Njiraini23 <njirainii88@gmail.com>
2023-05-19 19:49:02 +0000

    1-binary.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,53 @@
+#include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* binary_search - searches the value in a sortted integers
+*@array:  pointer to the first element of the array to search in
+*@size: the number of elements in an array
+*@value: the value to search for
+* Return: where the index is located
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t half = size / 2;
+	size_t i;
+
+	if (array == NULL || size == 0)
+		return (-1);
+	print("searching in array");
+
+	for (i = 0; i < size; i++)
+		printf("%s %d", (i == 0) ? ":" : ",", array[i]);
+
+	printf("\n");
+
+	if (half && size % 2 == 0)
+		half--;
+	if (value == array[half])
+		return ((int)half);
+	if (value < array[half])
+		return (recursive_search(array, half, value));
+	half++;
+	return (recursive_search(array + half, size - half, value) + half);
+}
+
+/**
+*binary_search - binary search to return
+* the index of number
+*@array: input array
+*@size: the size of the array
+*@value: value to search in
+*Return: index of the number
+*/
+int binary_search(int *array, size_t size, int value)
+{
+	int index;
+
+	index = recursive_search(array, size, value);
+
+	if (index >= 0 && array[index] != value)
+		return (-1);
+	return (index);
+}
